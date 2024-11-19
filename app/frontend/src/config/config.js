7d880b39fa67f14acad99a2d741b18bef0fd982d
Ubuntu <ubuntu@vps-4bcc1bb9.vps.ovh.net>
2024-11-19 13:56:34 +0000

    Docker(#15) env and deploy

--- app/frontend/src/config/config.js
+++ app/frontend/src/config/config.js
@@ -1,9 +1,9 @@
-// Define the base URLs for your API and frontend
+// src/config/config.js
+
 const config = {
-    apiBaseUrl: 'http://backend:3000/api/',   // Base URL for API calls
-    frontendBaseUrl: 'http://backend:3000/api/frontend/',  // Base URL for frontend routes
-    apiV1: 'http://backend:3000/api/v1/'  // Base URL for frontend routes
-  };
-  
-  export default config;
-  
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL,   // Base URL for API calls
+  frontendBaseUrl: process.env.REACT_APP_FRONTEND_BASE_URL,  // Base URL for frontend routes
+  apiV1: process.env.REACT_APP_API_V1  // Base URL for frontend routes
+};
+
+export default config;
